6bbe62f4cfa9d5b728aaa6eb933688e52184fd8e
unibtc <unibtc@gmail.com>
2020-08-03 22:19:41 +0000

    : remove cors options

--- src/server.js
+++ src/server.js
@@ -7,15 +7,7 @@ const app = new Koa();
 app.poweredBy = false;
 app.proxy = true;
 app.use(require('koa-helmet')());
-app.use(require('@koa/cors')({
-  origin: '*',
-  credentials: false,
-  maxAge: 600,
-  allowMethods: 'GET,HEAD,POST,OPTIONS',
-  allowHeaders: [
-    'Content-Type', 'Accept', 'Keep-Alive', 'DNT', 'User-Agent', 'X-Requested-With', 'If-Modified-Since', 'Cache-Control', 'Content-Type', 'Range'
-  ]
-}));
+app.use(require('@koa/cors')());
 app.use(logger());
 app.use(bodyParser());
 require('./middleware')(app);
