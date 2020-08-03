0f82ca6988fc5f2bba8cff7ac5084ceda9743041
unibtc <unibtc@gmail.com>
2020-08-03 22:35:12 +0000

    : clean up

--- src/dfuse.js
+++ src/dfuse.js
@@ -7,23 +7,20 @@ const dfuseClient = createDfuseClient({
   apiKey: process.env.ETH_KEY,
   network: "mainnet.eth.dfuse.io",
   httpClientOptions: {
-      fetch: nodeFetch
-    },
-        graphqlStreamClientOptions: {
-      socketOptions: {
-        // The WebSocket factory used for GraphQL stream must use this special protocols set
-        // We intend on making the library handle this for you automatically in the future,
-        // for now, it's required otherwise, the GraphQL will not connect correctly.
-        webSocketFactory: (url) => webSocketFactory(url, ["graphql-ws"])
-      }
-    },
-streamClientOptions: {
-      socketOptions: {
-        webSocketFactory: (url) => webSocketFactory(url)
-      }
+    fetch: nodeFetch
+  },
+  graphqlStreamClientOptions: {
+    socketOptions: {
+      webSocketFactory: (url) => webSocketFactory(url, ["graphql-ws"])
     }
+  },
+  streamClientOptions: {
+    socketOptions: {
+      webSocketFactory: (url) => webSocketFactory(url)
+    }
+  }
 });
-async function webSocketFactory(url, protocols=[]) {
+async function webSocketFactory(url, protocols = []) {
   const webSocket = new WebSocketClient(url, protocols, {
     handshakeTimeout: 30 * 1000, // 30s
     maxPayload: 200 * 1024 * 1000 * 1000 // 200Mb
@@ -38,41 +35,24 @@ async function webSocketFactory(url, protocols=[]) {
 
   return webSocket
 }
-function parseTransaction(txobj) {
-  txobj = txobj.data.transaction;
-  console.log(txobj)
-  return {
-    blockHash: txobj.block.hash,
-    blockNumber: txobj.block.number,
-    from:txobj.from,
-    gas:txobj.gasLimit,
-    gasPrice:txobj.gasPrice,
-    hash:txobj.hash,
-    input:txobj.inputData,
-    nonce:txobj.nonce,
-    to:txobj.to,
-    value:txobj.value
-  }
- }
-
 
 exports.validateTx = async (txid) => {
   try {
-  const vars = {
+    const vars = {
       variables: {
         hash: txid
       },
       operationType: "query"
     }
 
-  const response = await dfuseClient.graphql(validate,vars)
-  if (response.errors) {
-    throw response.errors;
-  }
-  const valid = response.data.transaction.flatCalls.every((tx) => tx.status === 'SUCCEEDED');
-  dfuseClient.release();
-  return valid ? 'SUCCEEDED': 'REVERTED';
-  }catch(e) {
+    const response = await dfuseClient.graphql(validate, vars)
+    if (response.errors) {
+      throw response.errors;
+    }
+    const valid = response.data.transaction.flatCalls.every((tx) => tx.status === 'SUCCEEDED');
+    dfuseClient.release();
+    return valid ? 'SUCCEEDED' : 'REVERTED';
+  } catch (e) {
     console.error(e)
     // if txid is not yet confirmed
     return 'UNCONFIRMED';
